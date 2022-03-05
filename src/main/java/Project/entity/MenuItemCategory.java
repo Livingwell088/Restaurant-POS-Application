75b15b062f25857c79346675e318b2e233bc5658
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-03-04 23:49:33 -0500

    Added menuitem and menuitemcategory class along with constructor, getters and setters

--- src/main/java/Project/entity/MenuItemCategory.java
+++ src/main/java/Project/entity/MenuItemCategory.java
@@ -8,5 +8,53 @@ public class MenuItemCategory {
     private String specialRequest;
     private int menuType;
 
+    public MenuItemCategory(String firstName, String secondName, int category, String specialRequest, int menuType) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.category = category;
+        this.specialRequest = specialRequest;
+        this.menuType = menuType;
+    }
 
+    public MenuItemCategory() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getSpecialRequest() {
+        return specialRequest;
+    }
+
+    public void setSpecialRequest(String specialRequest) {
+        this.specialRequest = specialRequest;
+    }
+
+    public int getMenuType() {
+        return menuType;
+    }
+
+    public void setMenuType(int menuType) {
+        this.menuType = menuType;
+    }
 }
