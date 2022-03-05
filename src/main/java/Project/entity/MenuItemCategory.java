32728e1871a3509ff31a8a695bd0ad9164640312
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-03-04 23:33:01 -0500

    added menu item category class

---
+++ src/main/java/Project/entity/MenuItemCategory.java
@@ -0,0 +1,12 @@
+package Project.entity;
+
+public class MenuItemCategory {
+
+    private String firstName;
+    private String secondName;
+    private int category;
+    private String specialRequest;
+    private int menuType;
+
+
+}
