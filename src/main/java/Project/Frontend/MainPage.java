7f5f02503fadb01a23b995e70832e4bfcbe7cb41
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-05-12 08:52:47 -0400

    Buttons done for edit orders screen. Started working on set delivery screen.

--- src/main/java/Project/Frontend/MainPage.java
+++ src/main/java/Project/Frontend/MainPage.java
@@ -95,6 +95,7 @@ public class MainPage extends JFrame {
         menuButton.setIcon(menu);
         menuButton.setVerticalTextPosition(JButton.TOP);
         menuButton.setHorizontalTextPosition(JButton.CENTER);
+        menuButton.addActionListener(e -> onMenuButton());
 
         JButton configButton = new JButton();
         configButton.setBackground(Color.WHITE);
@@ -160,24 +161,28 @@ public class MainPage extends JFrame {
         walkIn.setText("WALK IN");
         walkIn.setHorizontalTextPosition(JButton.CENTER);
         walkIn.setBackground(new Color(251, 208, 208));
+        walkIn.setFocusable(false);
         walkIn.setVisible(true);
 
         dineIn.setBounds(15, 15, dineInPanel.getWidth() - 30, dineInPanel.getHeight() - 30);
         dineIn.setText("DINE IN ");
         dineIn.setHorizontalTextPosition(JButton.CENTER);
         dineIn.setBackground(new Color(251, 208, 208));
+        dineIn.setFocusable(false);
         dineIn.setVisible(true);
 
         delivery.setBounds(15, 15, deliveryPanel.getWidth() - 30, deliveryPanel.getHeight() - 30);
         delivery.setText("DELIVERY");
         delivery.setHorizontalTextPosition(JButton.CENTER);
         delivery.setBackground(new Color(251, 208, 208));
+        delivery.setFocusable(false);
         delivery.setVisible(true);
 
         pickUp.setBounds(15, 15, pickUpPanel.getWidth() - 30, pickUpPanel.getHeight() - 30);
         pickUp.setText("PICK UP");
         pickUp.setHorizontalTextPosition(JButton.CENTER);
         pickUp.setBackground(new Color(251, 208, 208));
+        pickUp.setFocusable(false);
         pickUp.setVisible(true);
 
         walkInPanel.setLayout(null);
@@ -206,5 +211,135 @@ public class MainPage extends JFrame {
         orderFrame.add(deliveryPanel);
         orderFrame.add(pickUpPanel);
         orderFrame.setVisible(true);
+    }
+
+    private void onMenuButton() {
+        int width = Toolkit.getDefaultToolkit().getScreenSize().width - 200;
+        int height = Toolkit.getDefaultToolkit().getScreenSize().height - 200;
+
+        JPanel drawerPanel = new JPanel();
+        JPanel driverPanel = new JPanel();
+        JPanel editOrderPanel = new JPanel();
+        JPanel modifyPanel = new JPanel();
+        JPanel switchLanguagePanel = new JPanel();
+        JPanel paymentPanel = new JPanel();
+
+        drawerPanel.setBackground(new Color(83, 72, 89));
+        driverPanel.setBackground(new Color(83, 72, 89));
+        editOrderPanel.setBackground(new Color(83, 72, 89));
+        modifyPanel.setBackground(new Color(83, 72, 89));
+        switchLanguagePanel.setBackground(new Color(83, 72, 89));
+        paymentPanel.setBackground(new Color(83, 72, 89));
+
+        int panelWidth = (width / 6) - 40;
+        int panelX = (width / 6);
+        drawerPanel.setBounds(20, 150, panelWidth, height - 350);
+        driverPanel.setBounds(panelX + 20, 150, panelWidth, height - 350);
+        editOrderPanel.setBounds((2 * panelX) + 20, 150, panelWidth, height - 350);
+        modifyPanel.setBounds((3 * panelX) + 20, 150, panelWidth, height - 350);
+        switchLanguagePanel.setBounds((4 * panelX) + 20, 150, panelWidth, height - 350);
+        paymentPanel.setBounds((5 * panelX) + 20, 150, panelWidth, height - 350);
+
+        JButton drawer = new JButton();
+        JButton driver = new JButton();
+        JButton editOrder = new JButton();
+        JButton modify = new JButton();
+        JButton switchLanguage = new JButton();
+        JButton payment = new JButton();
+
+        drawer.setBounds(15, 15, drawerPanel.getWidth() - 30, drawerPanel.getHeight() - 30);
+        drawer.setText("OPEN DRAWER");
+        drawer.setHorizontalTextPosition(JButton.CENTER);
+        drawer.setBackground(new Color(251, 208, 208));
+        drawer.setFocusable(false);
+        drawer.setVisible(true);
+
+        driver.setBounds(15, 15, driverPanel.getWidth() - 30, driverPanel.getHeight() - 30);
+        driver.setText("SET DRIVER");
+        driver.setHorizontalTextPosition(JButton.CENTER);
+        driver.setBackground(new Color(251, 208, 208));
+        driver.setFocusable(false);
+        driver.addActionListener(e -> setDeliveryScreen());
+        driver.setVisible(true);
+
+        editOrder.setBounds(15, 15, editOrderPanel.getWidth() - 30, editOrderPanel.getHeight() - 30);
+        editOrder.setText("EDIT ORDER");
+        editOrder.setHorizontalTextPosition(JButton.CENTER);
+        editOrder.setBackground(new Color(251, 208, 208));
+        editOrder.setFocusable(false);
+        editOrder.setVisible(true);
+
+        modify.setBounds(15, 15, modifyPanel.getWidth() - 30, modifyPanel.getHeight() - 30);
+        modify.setText("MODIFY ORDER");
+        modify.setHorizontalTextPosition(JButton.CENTER);
+        modify.setBackground(new Color(251, 208, 208));
+        modify.setFocusable(false);
+        modify.setVisible(true);
+
+        switchLanguage.setBounds(
+                15, 15, switchLanguagePanel.getWidth() - 30, switchLanguagePanel.getHeight() - 30);
+        switchLanguage.setText("<html> SWITCH<br/>LANGUAGE");
+        switchLanguage.setHorizontalTextPosition(JButton.CENTER);
+        switchLanguage.setBackground(new Color(251, 208, 208));
+        switchLanguage.setFocusable(false);
+        switchLanguage.setVisible(true);
+
+        payment.setBounds(15, 15, paymentPanel.getWidth() - 30, paymentPanel.getHeight() - 30);
+        payment.setText("PAYMENT");
+        payment.setHorizontalTextPosition(JButton.CENTER);
+        payment.setBackground(new Color(251, 208, 208));
+        payment.setFocusable(false);
+        payment.setVisible(true);
+
+        drawerPanel.setLayout(null);
+        drawerPanel.add(drawer);
+
+        driverPanel.setLayout(null);
+        driverPanel.add(driver);
+
+        editOrderPanel.setLayout(null);
+        editOrderPanel.add(editOrder);
+
+        modifyPanel.setLayout(null);
+        modifyPanel.add(modify);
+
+        switchLanguagePanel.setLayout(null);
+        switchLanguagePanel.add(switchLanguage);
+
+        paymentPanel.setLayout(null);
+        paymentPanel.add(payment);
+
+        JFrame editFrame = new JFrame();
+        editFrame.setTitle("Place Order");
+        editFrame.setDefaultCloseOperation(editFrame.DISPOSE_ON_CLOSE);
+
+        System.out.println(width);
+        editFrame.setSize(width, height);
+        editFrame.setResizable(false);
+        editFrame.setLocationRelativeTo(null);
+        editFrame.setLayout(null);
+        editFrame.setBackground(new Color(251, 240, 232));
+        editFrame.add(drawerPanel);
+        editFrame.add(driverPanel);
+        editFrame.add(editOrderPanel);
+        editFrame.add(modifyPanel);
+        editFrame.add(switchLanguagePanel);
+        editFrame.add(paymentPanel);
+
+        editFrame.setVisible(true);
+    }
+
+    private void setDeliveryScreen() {
+        JFrame deliveryScreen = new JFrame();
+
+        deliveryScreen.setTitle("Set Delivery Drivers");
+        deliveryScreen.setDefaultCloseOperation(deliveryScreen.DISPOSE_ON_CLOSE);
+
+        deliveryScreen.setSize(
+                Toolkit.getDefaultToolkit().getScreenSize().width,
+                Toolkit.getDefaultToolkit().getScreenSize().height);
+        deliveryScreen.setExtendedState(deliveryScreen.MAXIMIZED_BOTH);
+        deliveryScreen.setResizable(false);
+        deliveryScreen.setVisible(true);
     }
 }
